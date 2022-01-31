fdaf785f1899340396ba4ffed437fddbf1e91f12
evlas1013 <nick.salve@gmail.com>
2022-01-31 12:19:42 -0500

    code generator
    HTML writes py code for any number of IO

--- WriteKeys.js
+++ WriteKeys.js
@@ -1,12 +1,64 @@
-var startIndex = 8;
-var endIndex = 19;
-for(var i = startIndex; i<endIndex; i++)
+var startIndex = 0;
+var endIndex = 0;
+var output = "";
+var keyCodes = 
 {
-	console.log("b" + i +" = digitalio.DigitalInOut(board.GP"+i+")");
-	console.log("b" + i +".switch_to_input(pull=digitalio.Pull.DOWN)");
-
-	console.log("if b"+i+".value and (lastPressed != "+i+" or currentDelay > buttonDelay):");
-	console.log("k.send(Keycode.LEFT_CONTROL, Keycode.LEFT_ALT, Keycode.ZERO)");
-	console.log("lastPressed = "+i);
-	console.log("currentDelay = 0");
+0: "ZERO",
+1: "ONE",
+2: "TWO",
+3: "THREE",
+4: "FOUR",
+5: "FIVE",
+6: "SIX",
+7: "SEVEN",
+8: "EIGHT",
+9: "NINE",
+10: "ZERO",
+11: "ONE",
+12: "TWO",
+13: "THREE",
+14: "FOUR",
+15: "FIVE",
+16: "SIX",
+17: "SEVEN",
+18: "EIGHT",
+19: "NINE"
+}
+function generate(){
+	output = "";
+	endIndex = parseInt(document.getElementById("numberOfButtons").value) - 1;
+	output += "import board\n";
+	output += "import usb_hid\n";
+	output += "import digitalio\n";
+	output += "import time\n";
+	output += "from adafruit_hid.Keyboard import Keyboard\n";
+	output += "from adafruit_hid.keycode import Keycode\n";
+	output += 'print("running")\n';
+	output += "k = Keyboard(usb_hid.devices)\n\n";
+	output += "#INIT GPIO\n";
+	for(var i = startIndex; i<=endIndex; i++)
+	{
+		output += "b" + i +" = digitalio.DigitalInOut(board.GP"+i+")\n";
+		output += "b" + i +".switch_to_input(pull=digitalio.Pull.DOWN)\n\n";
+	}
+	output += "#LISTENERS\n"
+	output += "buttonDelay = 50\n";
+	output += "currentDelay = 0\n";
+	output += "lastPressed = -1\n";
+	output += "while True:\n";
+	output += "    time.sleep(0.01)\n";
+	output += "    currentDelay += 1\n";
+	output += "    #print(lastPressed)\n";
+	for(var i = startIndex; i<=endIndex; i++)
+	{
+		output += "    if b"+i+".value and (lastPressed != "+i+" or currentDelay > buttonDelay):\n";
+		output += "        (k.send(Keycode.LEFT_CONTROL, Keycode.LEFT_ALT, ";
+		if(i>=10)
+			output += "Keycode.LEFT_SHIFT, ";
+		output += "Keycode."+keyCodes[i]+")\n";
+		output += "        lastPressed = "+i+"\n";
+		output += "        currentDelay = 0\n";
+		output += "        continue\n\n";
+	}
+	document.getElementById("myOutput").value = output;
 }
